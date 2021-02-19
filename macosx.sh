ebde4d1800720a092434e3e72066904586b817e1
Joanna Delaporte <joannadelaporte@gmail.com>
2021-02-19 11:01:27 -0600

    Update macosx.sh

--- macosx.sh
+++ macosx.sh
@@ -4,3 +4,10 @@
 #Install virtualbox
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# virtualbox requires a kernel extension to work.
+# If the installation fails, retry after you enable it in:
+# System Preferences → Security & Privacy → General
+
+brew install virtualbox
+brew install vagrant
+brew install vagrant-manager
